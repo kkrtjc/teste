92e7d46eecf61c04a34ea3cd3012b2879b343f19
kkrtjc <joaopaulojaguar@icloud.com>
2026-01-17 11:51:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -212,6 +212,7 @@ function renderOrderBumps(bumps) {
         if (!imgSrc) {
             if (bump.id === 'ebook-doencas' || bump.id === 'bump-doencas') imgSrc = 'capadasdoencas.png';
             else if (bump.id === 'ebook-manejo' || bump.id === 'bump-manejo') imgSrc = 'capadospintinhos.png';
+            else if (bump.id === 'bump-6361') imgSrc = 'tabela_racao_v2.png';
         }
 
         return `
