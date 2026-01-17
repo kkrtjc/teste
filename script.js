5a01713cc401c59b19f32efd7101cdced48414ff
kkrtjc <joaopaulojaguar@icloud.com>
2026-01-16 22:48:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- 1. GLOBAL CONFIG & STATE ---
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
-    : window.location.origin;
+    : 'https://teste-m1kq.onrender.com';
 
 let cart = {
     mainProduct: null,
